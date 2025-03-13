ce96bb7b568e90b4da2b06729eb0b0d65cedf755
Ayisha <aishacmela@gmail.com>
2025-03-13 14:43:26 +0200

    created correlation with seaborn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt 
+import seaborn as sns
+import numpy as np
 
 #load the dataset
 df = pd.read_csv("data/data.csv")
@@ -17,15 +19,29 @@ plt.ylabel("Amount (R)")
 plt.title("Sales & Profit Over Time")
 plt.legend()
 
+#compute correlation matrix 
+correlation = df.corr()
 
-#create bar chart
-plt.bar(df["Year"], df["Sales"], color="blue", label= "Sales")
-plt.bar(df["Year"], df["Profit"], color="red", label= "Profit", alpha=0.7)
-
-plt.xlabel("Sales (R)")
-plt.ylabel("Profit")
-plt.title("Sales vs Profit")
-
+#create a heatmap
+sns.heatmap(correlation, annot=True, cmap="coolwarm", linewidths=0.5)
 
 #Show plot 
+plt.title("Correlation Heatmap")
 plt.show()
+
+#create bar chart
+#plt.bar(df["Year"], df["Sales"], color="blue", label= "Sales")
+#plt.bar(df["Year"], df["Profit"], color="red", label= "Profit", alpha=0.7)
+#
+#plt.xlabel("Sales (R)")
+#plt.ylabel("Profit")
+#plt.title("Sales vs Profit")
+
+
+#create scatter plot 
+#plt.scatter(df["Sales"], df["Profit"], color="purple")
+#plt.xlabel("Sales (R)")
+#plt.ylabel("Profit")
+#plt.title("Sales vs Profit")
+
+
