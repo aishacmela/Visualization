b36783e3296fa66bee2b5eb64b8ef6fc88e322c4
Ayisha <aishacmela@gmail.com>
2025-03-13 14:12:58 +0200

    basic line plot wit matplotlib

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
 
 #load the dataset
 df = pd.read_csv("data/data.csv")
 
 #display the first rows
 print(df.head())
+
+#create line plot
+plt.plot(df["Year"], df["Sales"], marker = "o", linestyle = "-", color="b", label="Sales")
+plt.plot(df["Year"], df["Profit"], marker = "s", linestyle = "--", color="r", label="Profit")
+
+#customize the plot 
+plt.xlabel("Year")
+plt.ylabel("Amount (R)")
+plt.title("Sales & Profit Over Time")
+plt.legend()
+plt.grid()
+
+
+#Show plot 
+plt.show()
