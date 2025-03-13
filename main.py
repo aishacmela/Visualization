e34da0983f9c5e05b8009f22bdeb3613f9b8753f
Ayisha <aishacmela@gmail.com>
2025-03-13 14:23:42 +0200

    basic bar graph with matplotlib

--- main.py
+++ main.py
@@ -8,15 +8,23 @@ df = pd.read_csv("data/data.csv")
 print(df.head())
 
 #create line plot
-plt.plot(df["Year"], df["Sales"], marker = "o", linestyle = "-", color="b", label="Sales")
-plt.plot(df["Year"], df["Profit"], marker = "s", linestyle = "--", color="r", label="Profit")
+plt.plot(df["Year"], df["Sales"], marker = "o", linestyle = "-", color="orange", label="Sales")
+plt.plot(df["Year"], df["Profit"], marker = "s", linestyle = "--", color="green", label="Profit")
 
 #customize the plot 
 plt.xlabel("Year")
 plt.ylabel("Amount (R)")
 plt.title("Sales & Profit Over Time")
 plt.legend()
-plt.grid()
+
+
+#create bar chart
+plt.bar(df["Year"], df["Sales"], color="blue", label= "Sales")
+plt.bar(df["Year"], df["Profit"], color="red", label= "Profit", alpha=0.7)
+
+plt.xlabel("Sales (R)")
+plt.ylabel("Profit")
+plt.title("Sales vs Profit")
 
 
 #Show plot 
