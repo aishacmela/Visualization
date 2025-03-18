89b641501a0388150466271953af6113bdd8668d
Ayisha <aishacmela@gmail.com>
2025-03-18 14:13:30 +0200

    added cfu count graph

--- triplicates.py
+++ triplicates.py
@@ -2,15 +2,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Manually entering the data (from the table)
+# Manually entering the data (from the image)
 data = {
     "X": [0, 3, 6, 9, 12, 18, 24],
-    "A:Y1": [0.447, 0.697, 0.955, 1.102, 1.415, 1.914, 1.924],
-    "A:Y2": [0.506, 0.448, 0.966, 1.029, 1.572, 1.903, 1.935],
-    "A:Y3": [0.478, 0.452, 0.970, 1.343, 1.674, 1.866, 1.909],
-    "B:Y1": [0.303, 0.524, 0.550, 1.002, 1.474, 1.931, 1.916],
-    "B:Y2": [0.327, 0.541, 0.560, 0.994, 1.332, 1.962, 1.897],
-    "B:Y3": [0.492, 0.540, 0.610, 0.920, 1.333, 1.912, 1.914],
+    "A:Y1": [10000, 35000, 340000, 3500000, 3e+007, 1.87e+008, 1.9e+008],
+    "A:Y2": [16000, 43000, 370000, 4100000, 3e+007, 2.75e+008, 2.8e+008 ],
+    "A:Y3": [5000, 87000, 400000, 3700000, 3e+007, 3.08e+008, 1.9e+008],
+    "B:Y1": [32000, 79000, 370000, 6500000, 3.3e+007, 3.3e+008, 8e+008],
+    "B:Y2": [16000, 71000, 410000, 4300000, 3.2e+007, 6.16e+008, 7.4e+008],
+    "B:Y3": [48000, 45000, 310000, 2.8e+007, 3.2e+007, 5.61e+008, 2.2e+008],
 }
 
 # Create DataFrame
@@ -23,17 +23,16 @@ df["A_Std"] = df[["A:Y1", "A:Y2", "A:Y3"]].std(axis=1)
 df["B_Mean"] = df[["B:Y1", "B:Y2", "B:Y3"]].mean(axis=1)
 df["B_Std"] = df[["B:Y1", "B:Y2", "B:Y3"]].std(axis=1)
 
-# Plot with error bars
+# Plot mean with error bars
 plt.figure(figsize=(8, 5))
-plt.errorbar(df["X"], df["A_Mean"], yerr=df["A_Std"], fmt='o-', label="sc (A)", capsize=4, color='b')
+plt.errorbar(df["X"], df["A_Mean"], yerr=df["A_Std"], fmt='o-', label="sc (A)", capsize=6, color='b')
 plt.errorbar(df["X"], df["B_Mean"], yerr=df["B_Std"], fmt='o-', label="hg (B)", capsize=3, color='r')
 
 # Customize the plot
 plt.xlabel("Time (Hours)")  
-plt.ylabel("OD600")
+plt.ylabel("CFU/ml")
 plt.title("Growth curve of sc & hg")
 plt.legend()
-
 
 # Show the plot
 plt.show()
